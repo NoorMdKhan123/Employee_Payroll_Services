fe1a13326d2d9fe1da2ec9238b69ea9236a571a5
NoorMdKhan123 <noormd6400@gmail.com>
2021-11-10 15:18:32 +0530

    CRUD operation

--- EmployeePayrollServices/payroll_services.sql
+++ EmployeePayrollServices/payroll_services.sql
@@ -1,9 +1,17 @@
 create database payroll_services;
 use payroll_services;
-create Table emploee_payroll 
+create table employee_services
 (
 id int identity not null,
 name varchar(150) not null,
 salary decimal not null,
 start date not null
 )
+insert into employee_services (name, salary, start) values ('rekha', 20000, '2021-10-09'), 
+('rish', 50000, '2021-10-08'),
+('anu', 10000, '2021-10-07'),
+('guari', 19000, '2021-10-05');
+
+
+
+
