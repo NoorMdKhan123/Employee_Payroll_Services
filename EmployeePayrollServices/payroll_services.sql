4dfb215bc80e8aaf565c5e6af2659bed4da4a1bb
NoorMdKhan123 <noormd6400@gmail.com>
2021-11-10 14:36:57 +0530

    Done with creation of database

--- EmployeePayrollServices/payroll_services.sql
+++ EmployeePayrollServices/payroll_services.sql
@@ -1,9 +1,3 @@
 create database payroll_services;
 use payroll_services;
-create Table emploee_payroll 
-(
-id int identity not null,
-name varchar(150) not null,
-salary decimal not null,
-start date not null
-)
+
